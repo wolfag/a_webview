cea1e1ac84182d82b4f740aef543bbc5ccc96ef7
Tai Nguyen <tai.nguyen@aspireapp.com>
2022-09-24 14:17:49 +0700

    Initial Commit

---
+++ H4kEr/H4kErApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4kErApp.swift
+//  H4kEr
+//
+//  Created by TaiNguyen on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct H4kErApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
